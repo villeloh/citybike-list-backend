052dfbf4edcbe15de93a005d4757ee3aeb782159
Ville Lohkovuori <villelohkovuori1@gmail.com>
2022-08-11 09:51:39 +0300

    clean up url constants. start crafting API endpoints.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { createAndPopulateDb } = require('./db/dbService');
+const { STATIONS_URL, TRIPS_URL, STATION_INFO_URL, TRIP_URL } = require('./constants');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get(STATIONS_URL, (req, res) => {
+  response.send("<h1>About</h1>");
+});
+
+app.get(TRIPS_URL, (req, res) => {
+
+});
+
+app.get(STATION_INFO_URL, (req, res) => {
+
+});
+
+app.post(STATION_URL, (req, res) => {
+
+});
+
+app.post(TRIP_URL, (req, res) => {
+
+});
+
+app.delete(STATION_URL, (req, res) => {
+
+});
+
+app.delete(TRIP_URL, (req, res) => {
+  
 });
 
 app.listen(port, () => {
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,14 @@
 
 exports.DB_NAME = 'citybikeDb';
 exports.DB_URL = 'mongodb://localhost:27017';
-exports.MIN_TRIP_DISTANCE_M = 100;
-exports.MIN_TRIP_DURATION_S = 10;
 exports.STATION_DATA_FILEPATH = './csv/Stations.csv';
 exports.TRIP_DATA_FILEPATHS = ['./csv/Trips-2021-05.csv' , './csv/Trips-2021-06.csv', './csv/Trips-2021-07.csv'];
 exports.DB_COLL_NAME_STATIONS = 'STATIONS';
 exports.DB_COLL_NAME_TRIPS = 'TRIPS';
+exports.MIN_TRIP_DISTANCE_M = 100;
+exports.MIN_TRIP_DURATION_S = 10;
+
+exports.STATIONS_URL = '/stations';
+exports.TRIPS_URL = '/trips';
+exports.STATION_INFO_URL = '/station';
+exports.TRIP_URL = '/trip';
